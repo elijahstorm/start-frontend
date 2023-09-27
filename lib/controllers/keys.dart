3dfd360be71c320c50b9a9b7e2ea3a344d105933
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-09-27 12:18:23 +0700

    nuke

--- lib/controllers/keys.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class GlobalKeys {
-  static GlobalKey<ScaffoldMessengerState> rootScaffoldMessengerKey =
-      GlobalKey<ScaffoldMessengerState>();
-}
--- lib/display/components/charts/charts.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:fl_chart/fl_chart.dart';
-// import 'package:flutter/material.dart';
-
-// import 'package:start_app_frontend/language/constants.dart';
-// import 'package:start_app_frontend/content/dashboard/content.dart';
-// import 'package:start_app_frontend/display/components/animations/loading.dart';
-
-// TODO: add charts
